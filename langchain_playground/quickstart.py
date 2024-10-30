74e5d2734b436ba9c3cb2a93ff040f1f7d30082b
Derek Damron <damrondm@gmail.com>
2024-10-29 22:57:38 -0700

    Add Retriever examples

--- langchain_playground/quickstart.py
+++ langchain_playground/quickstart.py
@@ -1,10 +1,18 @@
 # https://python.langchain.com/v0.1/docs/use_cases/chatbots/quickstart/
+# exec(open('quickstart.py').read())
 
 from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.memory import ChatMessageHistory
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_ollama.embeddings import OllamaEmbeddings
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from typing import Dict
+from langchain_core.runnables import RunnablePassthrough
 
 #####
 # Setup
@@ -89,3 +97,94 @@ chat_history.add_user_message("What did you just say?")
 second_history_response = chain.invoke({"messages": chat_history.messages})
 
 print(second_history_response.content)
+
+#####
+# Retrievers
+#
+
+loader = WebBaseLoader("https://docs.smith.langchain.com/overview")
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=OllamaEmbeddings(model="llama3"))
+
+retriever = vectorstore.as_retriever(k=4)
+
+docs = retriever.invoke("how can langsmith help with testing?")
+
+chat_docs = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+
+question_answering_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the user's questions based on the below context:\n\n{context}",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+document_chain = create_stuff_documents_chain(chat_docs, question_answering_prompt)
+
+demo_ephemeral_chat_history = ChatMessageHistory()
+
+demo_ephemeral_chat_history.add_user_message("how can langsmith help with testing?")
+
+document_chain_response = document_chain.invoke(
+    {
+        "messages": demo_ephemeral_chat_history.messages,
+        "context": docs,
+    }
+)
+
+print(document_chain_response)
+
+#####
+# Retrieval chain
+#
+
+def parse_retriever_input(params: Dict):
+    return params["messages"][-1].content
+
+retrieval_chain = RunnablePassthrough.assign(
+    context=parse_retriever_input | retriever,
+).assign(
+    answer=document_chain,
+)
+
+response = retrieval_chain.invoke(
+    {
+        "messages": demo_ephemeral_chat_history.messages,
+    }
+)
+
+print(response)
+
+demo_ephemeral_chat_history.add_ai_message(response["answer"])
+
+demo_ephemeral_chat_history.add_user_message("Can you elaborate more on performance monitoring with langsmith?")
+
+response2 = retrieval_chain.invoke(
+    {
+        "messages": demo_ephemeral_chat_history.messages,
+    },
+)
+
+print(response2['answer'])
+
+# If we want to pass in only the answer
+
+retrieval_chain_with_only_answer = (
+    RunnablePassthrough.assign(
+        context=parse_retriever_input | retriever,
+    )
+    | document_chain
+)
+
+retrieval_chain_with_only_answer.invoke(
+    {
+        "messages": demo_ephemeral_chat_history.messages,
+    },
+)
